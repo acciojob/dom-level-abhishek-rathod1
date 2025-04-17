03054ec0bae4c94c1c22adb80bcb6e13372b7fe7
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-17 17:12:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+let item = document.getElementById("level");
+let level = 0;
+
+while(item.parentNode && item.parentNode.nodeName !=="DOCUMENT"){
+	item = item.parentNode;
+	level++;
+}
+// d
+alert(level);
